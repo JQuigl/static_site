7568c41736783c2121638757cfb2c93e0ffc225d
JQuigl <quigley.jack1@gmail.com>
2025-07-21 16:29:20 -0500

    Converted to E2G-C inference site

---
+++ src/components/LanguageSelector.jsx
@@ -0,0 +1,17 @@
+const LANGUAGES = {
+    "English": "en",
+    "Irish": "ga",
+}
+
+export default function LanguageSelector({ type, onChange, defaultLanguage }) {
+    return (
+        <div className='language-selector'>
+            <label>{type}: </label>
+            <select onChange={onChange} defaultValue={defaultLanguage}>
+                {Object.entries(LANGUAGES).map(([key, value]) => {
+                    return <option key={key} value={value}>{key}</option>
+                })}
+            </select>
+        </div>
+    )
+}
